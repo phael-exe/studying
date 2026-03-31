ad7eb93712201c1a16ae55601d691f68d3d53db7
phael-exe <rapha555lima@gmail.com>
2026-03-30 22:26:41 -0300

    codigo de info da gpu via cuda

---
+++ cad/info.cu
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void printDevProp(cudaDeviceProp devProp)
+{
+    printf("Major revision number:         %d\n",  devProp.major);
+    printf("Minor revision number:         %d\n",  devProp.minor);
+    printf("Name:                          %s\n",  devProp.name);
+    printf("Total global memory:           %lu\n", devProp.totalGlobalMem);
+    printf("Total shared memory per block: %lu\n", devProp.sharedMemPerBlock);
+    printf("Total registers per block:     %d\n",  devProp.regsPerBlock);
+    printf("Warp size:                     %d\n",  devProp.warpSize);
+    printf("Maximum memory pitch:          %lu\n", devProp.memPitch);
+    printf("Maximum threads per block:     %d\n",  devProp.maxThreadsPerBlock);
+    for (int i = 0; i < 3; ++i)
+        printf("Maximum dimension %d of block:  %d\n", i, devProp.maxThreadsDim[i]);
+    for (int i = 0; i < 3; ++i)
+        printf("Maximum dimension %d of grid:   %d\n", i, devProp.maxGridSize[i]);
+    printf("Total constant memory:         %lu\n", devProp.totalConstMem);
+    printf("Texture alignment:             %lu\n", devProp.textureAlignment);
+    printf("Concurrent copy and execution: %s\n",  (devProp.asyncEngineCount > 0 ? "Yes" : "No"));
+    printf("Number of multiprocessors:     %d\n",  devProp.multiProcessorCount);
+    return;
+}
+
+int main()
+{
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    printf("CUDA Device Query...\n");
+    printf("There are %d CUDA devices.\n", devCount);
+
+    for (int i = 0; i < devCount; ++i)
+    {
+        printf("\nCUDA Device #%d\n", i);
+        cudaDeviceProp devProp;
+        cudaGetDeviceProperties(&devProp, i);
+        printDevProp(devProp);
+    }
+    return 0;
+}
